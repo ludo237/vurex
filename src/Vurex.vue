4ad440a2a117cf4469ed2d1e0aac4500a45f3bee
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-01-03 18:16:33 +0100

    Improve style

--- src/Vurex.vue
+++ src/Vurex.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="h-screen bg-gray-200">
-    <div class="h-full mx-auto flex justify-center items-center">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white px-6">
-        <header class="my-2">
+    <div class="mx-auto flex justify-center items-center pt-20">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4 mx-auto">
+        <header class="mb-2">
           <h1>Vurex</h1>
-          <small class="mt-0 mb-4 text-gray-600">Select a currency from the list and check the rates</small>
+          <small class="mt-0 text-gray-600">Select a currency from the list and check the rates</small>
         </header>
         <currency-selector />
         <rates v-if="hasSelectedCurrency" />
