4ff327ea81d7edb6c218dcf58eb8ef2ab17d549c
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-01-03 18:17:34 +0100

    Improve the selector

--- src/components/CurrencySelector.vue
+++ src/components/CurrencySelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="inline-block relative w-full">
+  <div class="inline-block relative w-full mb-2">
     <select
       @change="changeSelectedCurrency($event.target.value)"
       class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
