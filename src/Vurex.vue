a97e1bd1eff11b8fb9aa2266840e299ec473e6d8
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-01-03 13:00:43 +0100

    Scaffold the skeleton

---
+++ src/Vurex.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="h-screen bg-gray-200">
+    <div class="h-full mx-auto flex justify-center items-center">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white px-6">
+        <div class="py-2">
+          <div class="font-bold text-xl mb-1">Vurex</div>
+          <small class="text-gray-700 text-sm">List of all available currencies</small>
+        </div>
+        <form class="w-full max-w-sm">
+          <div class="flex items-center border-b border-b-2 border-teal-500 py-2">
+            <input class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 leading-tight focus:outline-none" type="text" placeholder="Search Currency" aria-label="`Search Query" />
+          </div>
+        </form>
+        <ul class="py-2">
+          <li>EUR</li>
+          <li>EUR</li>
+          <li>EUR</li>
+          <li>EUR</li>
+          <li>EUR</li>
+          <li>EUR</li>
+          <li>EUR</li>
+          <li>EUR</li>
+          <li>EUR</li>
+          <li>EUR</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'vurex',
+}
+</script>
+
+<style src="./assets/tailwind.css"/>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vurex from './Vurex.vue'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(Vurex)
 }).$mount('#app')
